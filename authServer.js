ccdf0bc740622b7bfb9e830cd433b1005698fa39
Joshua Kaplan <joshkap2015@gmail.com>
2021-02-04 23:06:14 +0100

    Adds user Mongoose schema for user

---
+++ authServer.js
@@ -0,0 +1,127 @@
+require('dotenv').config()
+const cors = require('cors');
+
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+const { restart } = require('nodemon')
+
+const mongoose = require('mongoose');
+
+// Bcrypt for password hashing and dehashing
+const bcrypt = require('bcrypt')
+// Rate limiting
+const rateLimiter = require("express-rate-limit");
+
+app.use("/users", rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 mins
+    max: 15 // limit each IP to 100 request per windowMs
+}))
+
+app.use(express.json())
+// middlewares - implement cors
+app.use(cors());
+
+// db mongoose connection to MongoDB
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
+.then(() => console.log('DB Connected'))
+.catch(err => console.log(err));
+
+// User creating users database
+const users = []
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+// Creating user with bcrypt hashed password
+app.post('/users', async (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      const user = { name: req.body.name, password: hashedPassword }
+      users.push(user)
+      console.log(users)
+      res.status(201).send()
+    } catch {
+      res.status(500).send()
+    }
+})
+
+// Authentication below
+
+let refreshTokens = []
+
+app.post(('/token'), (req,res) => {
+    const refreshToken = req.body.token
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        const accessToken = generateAccessToken({ name: user.name })
+        restart.json({ accessToken: accessToken })
+    })
+
+})
+
+app.delete('/logout', (req,res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token )
+    res.sendStatus(204)
+})
+
+// app.post('/login', (req,res) => {
+//     // Authenticates User
+
+//     const username = req.body.username
+//     const user = { name: username}
+
+//     const accessToken = generateAccessToken(user)
+//     const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+//     refreshTokens.push(refreshToken)
+//     res.json({ accessToken: accessToken, refreshToken: refreshToken })
+// })
+
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name === req.body.name)
+    if (user == null) {
+      return res.status(400).send('Cannot find user')
+      //return res.send('User not found!')
+    }
+    try {
+      if(await bcrypt.compare(req.body.password, user.password)) {
+
+        // Generating accessToken & refreshToken if user is found.
+        const accessToken = generateAccessToken(user)
+        const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+        refreshTokens.push(refreshToken)
+        // res.json({ accessToken: accessToken, refreshToken: refreshToken })
+        
+        res.status(200).json({
+          message: 'User logged in!',
+          accessToken: accessToken,
+          refreshToken: refreshToken,
+          username: user.name
+        })
+
+      } else {
+        res.status(400).json({
+          error: 'Password incorrect!'
+        })
+      }
+    } catch {
+      // Generic catch-all response
+      res.status(500).send('Lol')
+    }
+  })
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+}
+
+app.listen(4000)
---
+++ src/App.js
@@ -0,0 +1,87 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import {authenticate, logout, getUser } from './helpers.js';
+
+const  App = (props) => {
+
+  const [state, setState] = useState( {
+    username: '',
+    password: ''
+  })
+
+  const username = state.username
+  const password = state.password
+
+  function handleChange(stateProperty) {
+    return function(event) {
+      setState({ ...state, [stateProperty]: event.target.value })
+    };
+};
+
+    const handleSubmit = event => {
+      event.preventDefault();        
+
+        fetch('http://localhost:4000/users/login', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: username,
+                password: password
+            })
+        })
+        .then(response => {
+            if (response.ok) {
+              alert('User logged in successfully.')
+              // authenticate(response)
+            } else {
+              alert('User Not Found! Create a user first to log in.')
+            }
+            return response.json()
+        }).then(data => {
+          console.log(data)
+          // authenticate user and store token and user name in session storage
+          // authenticate(data)
+
+          // console.log(data.accessToken)
+          // console.log(data.username)
+
+          sessionStorage.setItem('token', JSON.stringify(data.accessToken))
+          sessionStorage.setItem('user', JSON.stringify(data.username))
+          // JSON.parse(sessionStorage.getItem('token'))
+          props.history.push('/')
+        })
+        .catch(error => console.log(error))
+    }
+
+  return (
+    
+    <div className="container">
+      { !getUser() && (
+        <div>        
+          <div>Log In</div>
+          <form onSubmit={handleSubmit}>
+            <label>Username</label>
+            <input type="text" value={username} name="username" onChange={handleChange('username')}></input>
+
+            <label>Password</label>
+            <input type="password" value={password} onChange={handleChange('password')} name="password"></input>
+
+            <button>Sign In</button>
+
+          </form>
+          <Link to="/signup">Don't have an account yet? Signup.</Link>
+        </div>
+      )}
+
+      { getUser() && (
+        <div>Hello <span style={{color:"red"}}>{getUser()}</span>! You are logged in already. <span onClick={() => logout(() => props.history.push('/'))}> Logout</span></div>
+      )}
+
+    </div>
+  );
+}
+
+export default withRouter(App);
