149144d1f15fffe38b06a5bae58122bd579b2386
Joshua Kaplan <joshkap2015@gmail.com>
2021-02-04 23:23:22 +0100

    Jsonwebtoken on user instance

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 var crypto = require('crypto'); // Using node built in crypto library for password hashing and salt generation
+var jwt = require('jsonwebtoken');
+require('dotenv').config()
 
 var UserSchema = new mongoose.Schema({
     username: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
@@ -13,6 +15,24 @@ var UserSchema = new mongoose.Schema({
 UserSchema.methods.setPassword = function(password){
       this.salt = crypto.randomBytes(16).toString('hex');
       this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    return this.hash === hash;
+};
+
+// generate JsonWebToken on user instance
+UserSchema.methods.generateJWT = function() {
+    var today = new Date();
+    var exp = new Date(today);
+    exp.setDate(today.getDate() + 60);
+
+    return jwt.sign({
+    id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000),
+    }, process.env.JWT_SECRET);
 };
     
 
