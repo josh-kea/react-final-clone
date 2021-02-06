66fa5614e3a66fe6c85ec5a9b48ebb8cb664543b
Joshua Kaplan <joshkap2015@gmail.com>
2021-02-06 20:12:50 +0100

    All users styling ddone

--- src/components/users/AllUsers.jsx
+++ src/components/users/AllUsers.jsx
@@ -33,6 +33,8 @@ const AllUsers = (props) => {
 
     return(
     <div id="AllUsers">
+        <h1>Users</h1>
+        <div className="user-rows">
             <div className="total-users-row">
                 <div>{users.length} Total Users</div>
                 <div>Sort By</div>
@@ -40,10 +42,15 @@ const AllUsers = (props) => {
             {
                 users.map((user, i) => {
                     return (
-                        <div className="user-row" key={user._id}>{user.email}</div>
+                        <Link to={`/admin/users/${user._id}`} className="user-row" key={user._id}>
+                            <div className="user-row-email">{user.email}</div>
+                            { user.isAdmin ? (<div className="row-badge admin-badge">Admin</div>) : (<div className="row-badge user-badge">User</div>) }
+                            { user.isValid ? (<div className="row-badge valid-email">Valid Email</div>) : (<div className="row-badge invalid-email">Invalid Email</div>) }
+                        </Link>
                     )
                 })
             }
+        </div>
 
 
 
