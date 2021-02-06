f8ffcf762bd80db46f9e08727a02c5a8da6a6a44
Joshua Kaplan <joshkap2015@gmail.com>
2021-02-06 17:08:08 +0100

    Adds Navbar for admin

--- src/AdminPage.js
+++ src/AdminPage.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { isAdmin } from './helpers.js';
 import { Link, withRouter } from 'react-router-dom';
+import AdminNav from './components/AdminNav';
+import AdminLeftPanel from './components/AdminLeftPanel';
 
 require("dotenv").config();
 
 const AdminPage = (props) => {
 
     return (
-    <div className="container">
-        <p>Hello</p>
+    <div id="AdminPage">
+        <AdminNav></AdminNav>
+        <AdminLeftPanel></AdminLeftPanel>
     </div>
   );
 };
---
+++ src/components/AdminNav.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link, withRouter } from 'react-router-dom'
+import { getUser, logout } from '../helpers'
+import './AdminNav.css'
+
+const AdminNav = (props) => {
+    return(
+    <nav className="navbar">
+        <ul>
+            <li >
+                <Link to="/admin" className="admin-badge">Product Research X</Link>
+            </li>
+            <li>
+                <Link to="/admin/products/new">Add New Product</Link>
+            </li>
+
+            {!getUser() && (
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+            )}
+
+            {getUser() && (
+                <li>
+                    <p onClick={() => logout(() => props.history.push('/'))}>Logout</p>
+                </li>
+            )}
+
+        </ul>
+
+    </nav> 
+    )
+}
+
+
+export default withRouter(AdminNav);
