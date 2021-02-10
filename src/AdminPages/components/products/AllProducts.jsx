2552bbb93e3ad44714cfa7b071badb5cb9e210de
Joshua Kaplan <joshkap2015@gmail.com>
2021-02-10 02:09:29 +0100

    Small touches

--- src/AdminPages/components/products/AllProducts.jsx
+++ src/AdminPages/components/products/AllProducts.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link, withRouter } from 'react-router-dom'
 import './AllProducts.css'
+import { ReactComponent as PlaceholderProductImg } from './PlaceholderProductImg.svg';
 
 import AddProductModal from './AddProductModal'
 
